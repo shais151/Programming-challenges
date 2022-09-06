280d93623a5f3e9971c5ccd0405b612e08ab7f52
shais151 <112864594+shais151@users.noreply.github.com>
2022-09-06 10:09:34 +0100

    Add files via upload

---
+++ Brampton Manor Academy/Challenge 1/testing.py
@@ -0,0 +1,13 @@
+import unittest
+
+from mycode import *
+
+
+class MyFirstTests(unittest.TestCase):
+
+    def test_hello(self):
+        self.assertEqual(hello_world(), 'hello world')
+
+    def test_custom_num_list(self):
+        self.assertEqual(len(create_list(10)), 10)
+        
