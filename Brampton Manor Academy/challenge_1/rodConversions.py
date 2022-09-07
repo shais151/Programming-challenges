edbe82ce65e3c4bca70c0f745700055e5bd7d728
shais151 <112864594+shais151@users.noreply.github.com>
2022-09-07 10:28:23 +0100

    Update rodConversions.py

--- Brampton Manor Academy/challenge_1/rodConversions.py
+++ Brampton Manor Academy/challenge_1/rodConversions.py
@@ -1,23 +1,23 @@
-def rodConversions():
-    string = ""
-    
-    value = float(input("Enter distance in rods: "))
-    
-    def rods_to_meters(value):
-        return value * 5.0292
-    def meters_to_feet(value):
-        return rods_to_meters(value) / 0.3048
-    def meters_to_miles(value):
-        return rods_to_meters(value) / 1609.34
-    def rods_to_furlongs(value):
-        return value / 40
-    def time_to_walk(value):
-        return (meters_to_miles(value) / 3.1) * 60
+def rods_to_meters(rods): 
+    return rods * 5.0292 
+def meters_to_feet(rods): 
+    return rods_to_meters(rods) / 0.3048 
+def meters_to_miles(rods): 
+    return rods_to_meters(rods) / 1609.34 
+def rods_to_furlongs(rods): 
+    return rods / 40 
+def time_to_walk(rods): 
+    return (meters_to_miles(rods) / 3.1) * 60 
 
-    print(f"Meters: {rods_to_meters(value)}")
-    print(f"Feet: {meters_to_feet(value)}")
-    print(f"Miles: {meters_to_miles(value)}")
-    print(f"Furlongs: {rods_to_furlongs(value)}")
-    print(f"Minutes to walk: {time_to_walk(value)}")
+def enter_rods():
+    return float(input("Enter distance in rods: ")) 
 
-rodConversions()
+def rodConversions(rods): 
+    print(f"Meters: {rods_to_meters(rods)}") 
+    print(f"Feet: {meters_to_feet(rods)}") 
+    print(f"Miles: {meters_to_miles(rods)}") 
+    print(f"Furlongs: {rods_to_furlongs(rods)}") 
+    print(f"Minutes to walk: {time_to_walk(rods)}") 
+
+rods = enter_rods()
+rodConversions(rods)
