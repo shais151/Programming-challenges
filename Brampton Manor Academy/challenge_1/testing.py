c4bb46e4e3083a5d066db79b9894a2d1d1b03210
shais151 <112864594+shais151@users.noreply.github.com>
2022-09-12 11:46:50 +0100

    Update testing.py

--- Brampton Manor Academy/challenge_1/testing.py
+++ Brampton Manor Academy/challenge_1/testing.py
@@ -1,15 +1,19 @@
 import unittest
 
-from mycode import *
-
+from rodConversions import *
 
 class MyFirstTests(unittest.TestCase):
 
-    def test_hello(self):
-        self.assertEqual(hello_world(), 'hello world')
-
-    def test_custom_num_list(self):
-        self.assertEqual(len(create_list(10)), 10)
-
+    def test_rodToMeter(self):
+        self.assertEqual(rods_to_meters(10), 50.292)
+    def test_rodToFurlong(self):
+        self.assertEqual(rods_to_furlongs(10), 0.25)
+    def test_meterToMile(self):
+        self.assertEqual(meters_to_miles(10), 0.03125007767159208)
+    def test_meterToFeet(self):
+        self.assertEqual(meters_to_feet(10), 165.0)
+    def test_speed(self):
+        self.assertEqual(time_to_walk(10), 0.6048402129985564)
+                         
 if __name__ == '__main__':
     unittest.main()
