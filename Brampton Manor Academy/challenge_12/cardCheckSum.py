f827c129bda02da4275eb91a29d2a4ca5088e101
shais151 <shais151.316@bramptonmanor.org.uk>
2022-11-02 10:44:25 +0000

    Update cardCheckSum.py

--- Brampton Manor Academy/challenge_12/cardCheckSum.py
+++ Brampton Manor Academy/challenge_12/cardCheckSum.py
@@ -1,18 +1,31 @@
-number = 0
-while number == 0:
-    try:
-        number = int(input("Enter your card number: "))
+def getCardNumber():
+    number = 0
+    while number == 0:
+        try:
+            number = int(input("Enter your card number: "))
 
-        if len(str(number)) > 16:
-            print("Card number is too long")
-            number = 0
-        elif len(str(number)) < 16:
-            print("Card number is too short")
-            number = 0
-    except:
-        print("Card number must be just numbers")
+            if len(str(number)) > 16:
+                print("Card number is too long")
+                number = 0
+            elif len(str(number)) < 16:
+                print("Card number is too short")
+                number = 0
+        except:
+            print("Card number must be just numbers")
 
-num_str = str(number)
-pan = int(num_str[7:])
+def identifyPAN(number):
+    num_str = str(number)
+    return int(num_str[7:])
 
-checksum = int(num_str[-1])
+def checksum(number):
+    num_str = str(number)
+    return int(num_str[-1])
+
+def identifyIssuer(number):
+    return
+
+if __name__ == "__main__":
+    number = getCardNumber()
+    pan = identifyPAN(number)
+    checksumDigit = checksum(number)
+    issuer = identifyIssuer(number)
